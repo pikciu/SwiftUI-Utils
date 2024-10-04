cb6992b8ad5b463fc1450439b6bca282040d283b
pikciu <tomasz.pikc@gmail.com>
2024-10-04 19:38:39 +0200

    add missing init

--- Sources/Navigation/Navigation.swift
+++ Sources/Navigation/Navigation.swift
@@ -3,6 +3,8 @@ import Observation
 @Observable
 public final class Navigation<T: Route> {
     public var stack = [T]()
+    
+    public init() { }
 
     public func push(_ route: T) {
         stack.append(route)
